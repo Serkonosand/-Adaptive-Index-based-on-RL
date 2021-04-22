7162c3c093e754626f2b914bb3cf3ef363efc622
SongShuHao <ssh20000511@SoHu.com>
2021-04-22 22:46:01 +0800

    04/22 work

--- env.py
+++ env.py
@@ -1,32 +1,37 @@
 """
 This part is Environment Function
 """ 
+from RL_main import QLearningTable
+"""
+用于文件操作
+"""
+from itertools import islice
 def get_env_feedback(S, A):
     """
-    action from ['BPLUS', 'HASH']
+    action from ['toBPLUS', 'toHASH']
     present index structure from ['BPLUS', 'HASH']
     query type from [0:point query ,1:others]
     State : (present index structure ,query type)
             e.g. ('BPLUS', 1)
     """
-    if A == 'HASH':
+    if A == 'toHASH':
         if S[1] == 0:
             if S[0] == 'BPLUS':
                 S_ = S 
                 S_[0] = 'HASH'
-                R = 1
+                R = 0
             else S[0] == 'HASH':
                 S_ = S
                 R = 1
         else:
                 S_ = S
                 R = 0
-    elif A == 'BPLUS':
+    elif A == 'toBPLUS':
         if S[1] == 1:
             if S[0] == 'HASH':
                 S_ = S 
                 S_[0] = 'BPLUS'
-                R = 1
+                R = 0
             else S[0] == 'BPLUS':
                 S_ = S
                 R = 1
@@ -34,11 +39,38 @@ def get_env_feedback(S, A):
                 S_ = S
                 R = 0
     return S_, R
-def updata_index_structure(S):
+
+    
+def updata_index_structure(structure):
     """
     change index structure
     """
-    if S[0] == 'BPLUS':
-        pass
-    elif S[0] == 'HASH':
-        pass
+    if structure == 'BPLUS':
+        print(structure)
+    elif structure == 'HASH':
+        print(structure)
+
+
+if __name__ == '__main__':
+    actions = ['toBPLUS', 'toHASH']
+    RL = QLearningTable(actions)
+    with open(FILEPATH, 'r') as f:
+        tmpdata = f.read().splitlines()
+    for episode in range(100):
+        # initial structure
+        structure = 'BPLUS'
+        for i in range((100)):
+            # RL choose action based on structure
+            A = RL.choose_action(str(structure))
+            S = (structure, int(tmpdata[episode * 100 + i]))
+            # RL get environment feedback
+            S_, R = get_env_feedback(S, A)
+
+            structure_ = S_[0]
+            # RL learn from this transition
+            RL.learn(str(structure), A, R, str(structure_))
+
+            # swap structure
+            structure = structure_
+        RL.print_qtable()
+        updata_index_structure(structure)
